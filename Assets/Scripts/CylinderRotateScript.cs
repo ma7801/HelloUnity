de7f0a2f453321317deb415c3c50026b570a5463
Mike <ma7801@gmail.com>
2018-06-27 09:46:50 -0700

    Initial Committ

---
+++ Assets/Scripts/CylinderRotateScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderRotateScript : MonoBehaviour
+{
+    // Controls rotation speed
+    public float xspeed = 1;
+    public float yspeed = 1;
+    public float zspeed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xspeed, yspeed, zspeed);
+  
+    }
+}
